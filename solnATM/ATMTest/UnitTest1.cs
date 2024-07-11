445c172ba9cb08c4a9d1856039386eb1867173c0
unknown <ponshriharini@gmail.com>
2024-07-11 11:37:53 +0530

    Test project added

---
+++ solnATM/ATMTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ATMTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
